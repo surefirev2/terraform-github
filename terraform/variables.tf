bc30101b6e0c1742791d0c03751d52630528eb22
Colin Hutchinson <chutchic@gmail.com>
2025-05-01 01:51:13 -0400

    fix(perms): attempt2 at fixing the permissions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,8 @@
 # variables.tf
 # No variables needed for this simple example
+
+variable "github_token" {
+  description = "GitHub token for authentication"
+  type        = string
+  sensitive   = true
+}
