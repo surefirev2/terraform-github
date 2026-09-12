04be638d2fa8c4ad402e6512566317a3401dc524
John <chutchic@gmail.com>
2026-09-12 01:18:26 +0000

    feat(terraform): require automerge-gate on math-desktop main

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -349,6 +349,7 @@ variable "branch_protection_status_checks" {
     hockeymind           = ["automerge-gate/all-passed"]
     github               = ["automerge-gate/all-passed"]
     "barn-league-hockey" = ["automerge-gate/all-passed"]
+    "math-desktop"       = ["automerge-gate/all-passed"]
     "surefire-dms"       = ["automerge-gate/all-passed"]
     "Meadowkin"          = ["automerge-gate/all-passed"]
     revive               = ["automerge-gate/all-passed"]
